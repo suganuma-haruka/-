210c9bc54ab32ec8fea3f41be3cecc8d78bdd141
haruka suganuma <haruka.b26@gmail.com>
2016-06-15 08:47:04 +0900

    6th comit

--- 売上集計システム/src/jp/co/iccom/suganuma_haruka/calculate_sales/UriageSystem.java
+++ 売上集計システム/src/jp/co/iccom/suganuma_haruka/calculate_sales/UriageSystem.java
@@ -1,14 +1,15 @@
 package jp.co.iccom.suganuma_haruka.calculate_sales;
 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UriageSystem {
 	public static void main(String[] args) {
-//		System.out.println(args[0]);
 		HashMap<String, String> map = new HashMap<String, String>();
 		try {
 			File branch = new File(args[0], "branch.lst");
@@ -33,13 +34,12 @@ public class UriageSystem {
 
 			br.close();
 
-		} catch(IOException a) {
+		} catch(IOException e) {
 			System.out.println("支店定義ファイルが存在しません");
-			System.out.println(a);
+			System.out.println(e);
 		}
 
 		System.out.println(map.entrySet());
-
 
 
 
@@ -48,10 +48,10 @@ public class UriageSystem {
 			File commodity = new File(args[0], "commodity.lst");
 			FileReader fr = new FileReader(commodity);
 			BufferedReader br = new BufferedReader(fr);
-			String t;
-			while((t = br.readLine()) != null) {
+			String s;
+			while((s = br.readLine()) != null) {
 
-				String[] items = t.split(",");
+				String[] items = s.split(",");
 				for(int i = 0; i < items.length; i++) {
 						System.out.println(items[i]);
 				}
@@ -67,9 +67,9 @@ public class UriageSystem {
 
 			br.close();
 
-		} catch(IOException b) {
+		} catch(IOException e) {
 			System.out.println("商品定義ファイルが存在しません");
-			System.out.println(b);
+			System.out.println(e);
 		}
 
 		System.out.println(map1.entrySet());
@@ -79,21 +79,52 @@ public class UriageSystem {
 		File file = new File(args[0]);
 		File files[] = file.listFiles();
 
+		ArrayList<String> rcdList = new ArrayList<String>();
+
 		for(int i = 0; i < files.length; i++) {
 			String str = files[i].getName();
 			if(str.matches("^\\d{8}.rcd$")) {
-//				System.out.println(files[i].getName());
 
 				String[] items = (files[i].getName()).split("\\.");
-				for(int j = 0; j < items.length; j++) {
-					System.out.println(items[j]);
+
+				rcdList.add(items[0]);
+			}
+		}
+
+		String max = rcdList.get(rcdList.size() - 1);
+		String min = rcdList.get(0);
+
+		int x = Integer.parseInt(max);
+		int y = Integer.parseInt(min);
+
+		if(x - y == rcdList.size() - 1) {
+			System.out.println(rcdList);
+		} else {
+			System.out.println("売上ファイル名が連番になっていません");
+		}
 
 
-//				int k = Integer.parseInt(items[j]);
-				for(int k = Integer.parseInt(items[j]); items[j].length - 1; k++);
 
+		ArrayList<Integer> IntegerList = new ArrayList<Integer>();
 
+		HashMap<String, String> map2 = new HashMap<String, String>();
 
+		try {
+			for(int i = 0; i < rcdList.size(); i++) {
+				File file1 = new File(args[0], rcdList.get(i) + ".rcd");
+				FileReader fr = new FileReader(file1);
+				BufferedReader br = new BufferedReader(fr);
+				String s;
+//				while((s = br.readLine()) != null) {
+//					System.out.println(s);
+
+					ArrayList list = new ArrayList();
+					list.add(br.readLine());
+					list.add(br.readLine());
+					list.add(br.readLine());
+					System.out.println(list.get(0));
+					System.out.println(list.get(1));
+					System.out.println(list.get(2));
 
 				}
 
@@ -101,15 +132,22 @@ public class UriageSystem {
 
 
 
-			}
+//				String[] items = list;
+//				for(int j = 0; j < items.length; j++) {
+////					System.out.println(items[j]);
+//				}
+//
+//				map2.put(items[0],items[3]);
+
+//				br.close();
+//			}
+
+		} catch(IOException e) {
+			System.out.println(e);
 		}
 
 
-
-
-
-
-
+//		System.out.println(map2.entrySet());
 
 
 
